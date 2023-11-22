8a137d573e337f99a7642c2d9ca35ae3a53df4f8
Tomas <tomasfreire@gmail.com>
2023-11-22 15:34:04 -0300

    styles: modal

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,8 +7,11 @@ const Contact = () => {
         <div className="contact-container">
             <div className="contact-text">
                 <h2>Contact Us</h2>
-                <p>Feel free to reach out for any inquiries about our products.</p>
-                {/* Add more text or a form here as needed */}
+                <p>Presidente Peron 328 #588</p>
+                <p>CABA - Argentina</p>
+                <p>Phone +54 911 4342 9073</p>
+                <p>WhatsApp <a href='https://api.whatsapp.com/send/?phone=5491127163700'>+5491127163700</a></p>
+                <p>WeChat Id <a href='weixin://dl/chat?SA14DeAgosto'>SA14DeAgosto</a></p>
             </div>
             <div className="contact-image">
                 <img src={mapImage} alt="Contact" />
